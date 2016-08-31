dc24c03e96f83b1a0149f96a5f84533e0f4deafd
Dan Gopstein <dan@gopstein.com>
2016-08-31 16:41:49 -0400

    Add confusion data to project

--- clustered-matched-pair.R
+++ clustered-matched-pair.R
@@ -1,3 +1,15 @@
 corr.matched <- function(x, group.name, pre.measure.name, post.measure.name) {
   
 }
+
+mcnemars.clustered.durkalski <- function (x, group.name, pre.measure.name, post.measure.name) {
+  
+    nk <- Reduce("+", abcd)
+  
+  bk <- abcd$TF
+  ck <- abcd$FT
+  
+  X2v <- sum( (1/nk)*(bk-ck) )^2/sum(((bk - ck) / nk)^2)
+  
+  X2v
+}
---
+++ data.R
@@ -0,0 +1,8 @@
+library(CorrBin)
+
+data(dehp)
+dehp
+data(egde)
+egde
+
+confusion <- read.csv("confusion.csv")
