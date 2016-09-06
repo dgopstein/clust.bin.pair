73ed7932a4ffe0d5ca4851492760d78732e20e3e
Dan Gopstein <dan@gopstein.com>
2016-09-06 14:15:02 -0400

    Revert "Test Yang equivalency"
    This reverts commit bf12000fb736c4e7f84f255628e45df02e486757.

--- R/obuchowski.R
+++ R/obuchowski.R
@@ -5,11 +5,6 @@ obuchowski.test <- function(x, group.names, pre.measure.name, post.measure.name)
   obuchowski.impl(z$bk, z$ck, z$nk, nrow(z))
 }
 
-x1 <- z$bk
-x2 <- z$ck
-N  <- z$nk
-m  <- nrow(z)
-
 obuchowski.impl <- function(x1, x2, N, m) {
   # Eqn (1)
   p.hat <- function(x, N) sum(x) / sum(N)
@@ -27,13 +22,5 @@ obuchowski.impl <- function(x1, x2, N, m) {
     var.hat(x, N, m) + var.hat(x1, N, m) - 2 * cov.hat(x, x1, N, m)
   
   # Eqn (6)
-  X2o.1 <- ((p.hat(x1, N) - p.hat(x2, N))^2) / var.hat.diff(x1, x2, N, m)
-  
-  # Yang Equivalency
-  # K <- m
-  # bk <- x1 / N
-  # ck <- x2 / N
-  # X2o.2 <- ((K - 1) / K) * (sum(bk - ck)^2) / sum((bk - ck)^2)
-  
-  X2o.1
+  ((p.hat(x1, N) - p.hat(x2, N))^2) / var.hat.diff(x1, x2, N, m)
 }
