de110e95edf167d726a24b7eb2ac88af9c3e7a84
Dan Gopstein <dan@gopstein.com>
2016-09-06 02:46:49 -0400

    Eliasziw passes tests

--- R/eliasziw.R
+++ R/eliasziw.R
@@ -6,52 +6,19 @@ mcnemars <- function(bk, ck) {
   (b - c)^2/(b + c)
 }
 
-eliasziw1.test <- function (x, group.names, pre.measure.name, post.measure.name) {
+eliasziw.test <- function (x, group.names, pre.measure.name, post.measure.name) {
   z <- results.to.contingency.cols(x, group.names, pre.measure.name, post.measure.name)
-  eliasziw1.impl(z$bk, z$ck)
+  
+  abcd <- z[,c("ak","bk","ck","dk")]
+  
+  eliasziw.impl(abcd)
 }
 
-eliasziw1.impl <-function (bk, ck) {
-  # Number of discordant answers per subject
-  Sk <- bk + ck
+eliasziw.impl <- function (abcd) {
+  bk <- abcd$bk
+  ck <- abcd$ck
   
-  # Number of subjects with discordant answer
-  Kd <- sum(Sk >= 1)
-  
-  # Number of subjects
-  #K <- nrow(Sk)
-  K <- length(Sk)
-  
-  p.bar <- sum(bk) / sum(Sk)
-  
-  S.bar <- (1/Kd) * sum(Sk)
-  
-  S0 <- S.bar - (sum((Sk - S.bar)^2) - (K - Kd)*(S.bar^2)) / (Kd * (Kd - 1) * S.bar)
-  
-  BMS <- (1/Kd) * sum( ifelse(Sk >= 1, ((( bk - Sk*p.bar )^2) / Sk), 0) )
-  WMS <- (1/(Kd * (S.bar - 1))) * sum( ifelse(Sk >= 1, (( bk * (Sk - bk) ) / Sk), 0) )
-  
-  rho.hat <- (BMS - WMS) / (BMS + (S0 - 1)*WMS)
-  
-  nc <- S0 + Kd*(S.bar - S0)
-  
-  X2m <- mcnemars(bk, ck)
-  
-  C.hat <- (1 + (nc - 1) * rho.hat)
-  
-  assertthat::are_equal((1 + (nc - 1) * rho.hat), sum(Sk*(1+(Sk - 1)*rho.hat)) / sum(Sk))
-  
-  X2ma <- X2m / C.hat
-  
-  X2ma
-}
-
-
-eliasziw2 <- function (abcd) {
-  bk <- abcd$TF
-  ck <- abcd$FT
-  
-  # Row-wise sum [2, 2, 2, ...]
+  # Row-wise sum
   nk <- Reduce("+", abcd)
 
   # Number of discordant answers per subject
@@ -72,38 +39,31 @@ eliasziw2 <- function (abcd) {
   
   abcd.mat <- data.matrix(abcd)
   
-  # Column-wise sum: TT:1678, TF:168, FT:845, FF:342
+  # Column-wise sum
   abcd.sum <- sapply(abcd, sum)
   
   P.hat <- abcd.sum / N
 
   nk_X_P.hat <-t(sapply(nk, function(x) x * P.hat))
-  dput(abcd.mat)
-  dput(nk_X_P.hat)
 
   BMSpooled <- (1 / K) * sum( (abcd.mat - nk_X_P.hat)^2 / nk )
   WMSpooled <- (1 / (K * (n.bar - 1))) * sum( ( abcd.mat *  as.vector(nk - abcd.mat)) / nk )
   
   rho.tilde.star <- (BMSpooled - WMSpooled) / (BMSpooled + (n0 - 1)*WMSpooled)
   
-  rho.tilde <- 1 / (1 + P.hat[['TF']]*(1 - rho.tilde.star)/rho.tilde.star
-                      + P.hat[['FT']]*(1 - rho.tilde.star)/rho.tilde.star)
+  rho.tilde <- 1 / (1 + P.hat[['bk']]*(1 - rho.tilde.star)/rho.tilde.star
+                      + P.hat[['ck']]*(1 - rho.tilde.star)/rho.tilde.star)
   
   S.bar <- (1/Kd) * sum(Sk)
   
   S0 <- S.bar - sum((Sk - S.bar)^2 - (K - Kd)*(S.bar^2)) / (Kd * (Kd - 1) * S.bar)
+  S0 <- S.bar - (sum((Sk - S.bar)^2) - (K - Kd)*(S.bar^2)) / (Kd * (Kd - 1) * S.bar)
   
   nc <- S0 + Kd*(S.bar - S0)
 
   C.hat <- 1 + (nc - 1) * rho.tilde
   
-  X2di <- mcnemars(abcd$TF, abcd$FT) / C.hat
+  X2di <- mcnemars(bk, ck) / C.hat
   
   X2di
 }
-
-abcd.nk <- Reduce('+', abcd)
-mcnemars(abcd$TF, abcd$FT)
-durkalski.impl(abcd.nk, abcd$TF, abcd$FT)
-eliasziw1.impl(abcd$TF, abcd$FT)
-eliasziw2(abcd)
--- tests/testthat/testCorrBinPaired.R
+++ tests/testthat/testCorrBinPaired.R
@@ -16,15 +16,15 @@ test_that("clustered methods are same order as McNemars", {
   mcnemar.chi2 <- mcnemars(abcd.data$bk, abcd.data$ck)
   durkalski.chi2  <-  durkalski.test(confusion, group.names, pre.measure.name, post.measure.name)
   obuchowski.chi2 <- obuchowski.test(confusion, group.names, pre.measure.name, post.measure.name)
-  eliasziw1.chi2  <-  eliasziw1.test(confusion, group.names, pre.measure.name, post.measure.name)
+  eliasziw.chi2   <-   eliasziw.test(confusion, group.names, pre.measure.name, post.measure.name)
   
   d.diff  <- abs(mcnemar.chi2 - durkalski.chi2 ) / mcnemar.chi2
   o.diff  <- abs(mcnemar.chi2 - obuchowski.chi2) / mcnemar.chi2
-  e1.diff <- abs(mcnemar.chi2 - eliasziw1.chi2 ) / mcnemar.chi2
+  e.diff  <- abs(mcnemar.chi2 - eliasziw.chi2  ) / mcnemar.chi2
   
   expect_lt(d.diff,  .2, "Durkalski and McNemar are relatively close")
   expect_lt(o.diff,  .2, "Obuchowski and McNemar are relatively close")
-  expect_lt(e1.diff, .2, "Eliasziw1 and McNemar are relatively close")
+  expect_lt(e.diff,  .2, "Eliasziw and McNemar are relatively close")
 })
 
 test_that("durkalski works with various input", {
@@ -47,12 +47,12 @@ test_that("obuchowski works with various input", {
   expect_equal(obuchowski.chi2.abcd,      obuchowski.res, tolerance = 0.01, scale = NULL, "abcd column input")
 })
 
-test_that("eliasziw1 works with various input", {
-  eliasziw1.res <- 11.27761
-  
-  eliasziw1.chi2.ungrouped <- eliasziw1.test(confusion, group.names, pre.measure.name, post.measure.name)
-  eliasziw1.chi2.abcd      <- eliasziw1.impl(abcd$bk, abcd$ck)
-  
-  expect_equal(eliasziw1.chi2.ungrouped, eliasziw1.res, tolerance = 0.01, scale = NULL, "ungrouped column input")
-  expect_equal(eliasziw1.chi2.abcd,      eliasziw1.res, tolerance = 0.01, scale = NULL, "abcd column input")
+test_that("eliasziw works with various input", {
+  eliasziw.res <- 8.757071
+
+  eliasziw.chi2.ungrouped <- eliasziw.test(confusion, group.names, pre.measure.name, post.measure.name)
+  eliasziw.chi2.abcd      <- eliasziw.impl(abcd.data[,c("ak", "bk", "ck", "dk")])
+
+  expect_equal(eliasziw.chi2.ungrouped, eliasziw.res, tolerance = 0.01, scale = NULL, "ungrouped column input")
+  expect_equal(eliasziw.chi2.abcd,      eliasziw.res, tolerance = 0.01, scale = NULL, "abcd column input")
 })
