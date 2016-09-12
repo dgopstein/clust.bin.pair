9d0d9fd9a617dd13929dd0e319d43ff85b7ef7e0
Dan Gopstein <dan@gopstein.com>
2016-09-11 21:01:27 -0400

    yang passes tests

--- R/yang.R
+++ R/yang.R
@@ -7,19 +7,15 @@ yang.test <- function(x, group.names, pre.measure.name, post.measure.name) {
 
 yang.impl <- function(nk, bk, ck) {
   N <- sum(nk)
+  K <- length(nk)
+  
   p1.tilde <- (1 / N) * sum(bk)
   p2.tilde <- (1 / N) * sum(ck)
-  
-  dput(N)
-  dput(p1.tilde)
-  dput(p2.tilde)
   
   X2mo <-
     ((K - 1) / K) *
       sum(bk - ck)^2 /
         ( (1/2) * sum( ((bk - ck) - nk*(p1.tilde - p2.tilde))^2  + (bk - ck)^2 ) )
-  
-  dput(paste("X2mo: ",X2mo))
   
   X2mo
 }
