c191aef00aa11249f634ea4d17c80123fa998ec9
Dan Gopstein <dan@gopstein.com>
2016-09-23 22:44:18 -0400

    simplify tests

--- R/durkalski.R
+++ R/durkalski.R
@@ -5,4 +5,4 @@ durkalski.test <- function(ak, bk, ck, dk)
   .durkalski.impl(ak+bk+ck+dk, bk, ck)
 
 .durkalski.impl <- function(nk, bk, ck)
-  sum( (1/nk)*(bk-ck) )^2/sum(((bk - ck) / nk)^2 )
+  sum( (bk-ck)/nk )^2 / sum( ((bk-ck)/nk)^2 )
